9c2a9b60376b233e2448e01f4c1b55d93bc5ae9d
Sumit Suresh Patil <sumit.patil6291@gmail.com>
2020-11-17 21:59:30 +0530

    modified

--- Python/BegineerToAdvanced/BMIcalculator.py
+++ Python/BegineerToAdvanced/BMIcalculator.py
@@ -2,7 +2,7 @@
 #Explanation: Enter height and weight, program will
 #calculate your BMI 
 ht=float(input("Enter your height in meters: "))
-wt=float(input("Enter your weight in KG"))
+wt=float(input("Enter your weight in KG: "))
 BMI=wt/ht**2
 print(f"Your BMI is: {BMI}")
 print("Now analyze your BMI through chart and take sutaible measures!")
