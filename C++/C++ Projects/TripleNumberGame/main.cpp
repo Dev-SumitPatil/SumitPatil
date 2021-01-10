8e7861d0bfbf7912e2d5e80e6a31348d4c991127
Sumit Suresh Patil <sumit.patil6291@gmail.com>
2021-01-10 10:12:51 +0530

    Image Processing Project

---
+++ C++/C++ Projects/TripleNumberGame/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{   string name;
+    int a,b,c;
+    cout << "Welcome to the Triple Number Game ! \nEnter your name : " << endl;
+    cin>>name;
+    cout<<"Hii "<<name<<" .Let's check how intelligent you are !"<<endl;
+    return 0;
+}
